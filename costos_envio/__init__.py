a706c2f26aae9e50f41f647794b8d8788315e151
moregain909 <juan@gregor.com.ar>
2023-11-16 16:22:52 -0300

    some minor changes

--- costos_envio/__init__.py
+++ costos_envio/__init__.py
@@ -1,8 +1,8 @@
-# For relative imports to work in Python 3.6
+
 import os, sys; 
 
 path2root = os.path.join(os.path.dirname(__file__), "..")
-#sys.path.append(path2root)
+sys.path.append(path2root)
 
 #print(sys.path.append(os.path.dirname(os.path.realpath(__file__))))
 
