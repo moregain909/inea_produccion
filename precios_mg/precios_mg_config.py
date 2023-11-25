8179a4b1b0063640b8d16fbdaadbec6afd2190d6
moregain909 <juan@gregor.com.ar>
2023-11-25 12:26:11 -0300

    Levanta credenciales de bases inea local y remota

--- precios_mg/precios_mg_config.py
+++ precios_mg/precios_mg_config.py
@@ -3,12 +3,14 @@ import os
 
 load_dotenv()
 
+# Database server INEA
 MYSQL_USER = os.getenv('INEA_DB_MYSQL_USER')
 MYSQL_PASS = os.getenv('INEA_DB_MYSQL_PASS')
 MYSQL_HOST = os.getenv('INEA_DB_MYSQL_HOST')
 MYSQL_PORT = os.getenv('INEA_DB_MYSQL_PORT')
 DB = os.getenv('INEA_DB_DATABASE')
 
+# Database de prueba local
 LOCAL_MYSQL_USER = os.getenv('INEA_LOCAL_DB_MYSQL_USER')
 LOCAL_MYSQL_PASS = os.getenv('INEA_LOCAL_DB_MYSQL_PASS')
 LOCAL_MYSQL_HOST = os.getenv('INEA_LOCAL_DB_MYSQL_HOST')
