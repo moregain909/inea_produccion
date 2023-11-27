321053d891e4c5467b536b5451ba2e9ed5f89f36
moregain909 <juan@gregor.com.ar>
2023-11-27 17:30:15 -0300

    Agrega scripts de bash

---
+++ bash_scripts/desarrollo_precios_mg.sh
@@ -0,0 +1,7 @@
+
+
+source /home/juan/inea/server_produccion/inea_produccion/env/bin/activate
+
+/home/juan/inea/server_produccion/inea_produccion/env/bin/python3.11 /home/juan/inea/server_produccion/inea_produccion/precios_mg/precios_mg.py
+
+
