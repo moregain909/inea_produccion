c82be8349facbf9090f3437bd483664780034bfa
moregain909 <juan@gregor.com.ar>
2023-11-28 12:03:29 -0300

    Agrega paths a sys

--- notion/__init__.py
+++ notion/__init__.py
@@ -1,1 +1,6 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+path2root = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(path2root)
+
 from notion.notion_helpers import *
