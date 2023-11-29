dd5f83b1c4c649f6a88238e4b91768f782b971bc
moregain909 <juan@gregor.com.ar>
2023-11-29 14:41:51 -0300

    produccion_productos_mg.sh Primer commit

---
+++ bash_scripts/produccion_productos_mg.sh
@@ -0,0 +1,4 @@
+source /home/inea/sistemas/inea_produccion/env/bin/activate
+
+/home/inea/sistemas/inea_produccion/env/bin/python3.11 /home/inea/sistemas/inea_produccion/precios_mg/productos_mg.py
+
