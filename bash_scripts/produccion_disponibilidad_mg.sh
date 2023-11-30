263063a0b22316bbcfe0ce888de8d1188d78ede2
moregain909 <juan@gregor.com.ar>
2023-11-30 12:45:07 -0300

    Corrije path produccion_disponibilidad_mg.sh

--- bash_scripts/produccion_disponibilidad_mg.sh
+++ bash_scripts/produccion_disponibilidad_mg.sh
@@ -1,8 +1,8 @@
 
 
-source /home/inea/inea_produccion/env/bin/activate
+source /home/inea/sistemas/inea_produccion/env/bin/activate
 
-/home/inea/inea_produccion/env/bin/python3.11 /home/inea/inea_produccion/precios_mg/disponibilidad_mg.py
+/home/inea/sistemas/inea_produccion/env/bin/python3.11 /home/inea/sistemas/inea_produccion/precios_mg/disponibilidad_mg.py
 
 
  
