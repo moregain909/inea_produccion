78fd71c1d15aa5082b8d59b4fab578d27923cbd6
Juan Gregor <juan@gregor.com.ar>
2023-12-15 22:53:45 -0300

    Cambia paths a absolutos

--- bash_scripts/preciosmg.sh
+++ bash_scripts/preciosmg.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 now="$(date)"
@@ -9,7 +8,7 @@ echo $now
 # echo "previo a abrir exe"
 
 #	abre exe "PoductosMG"
-mono ../executables/ProductosMG/ProductosMG.exe &
+mono /home/inea/sistemas/inea_produccion/executables/ProductosMG/ProductosMG.exe &
 # echo "exe abierto"
 
 #	pausa
@@ -20,7 +19,7 @@ sleep 4
 xdotool search --name "Productos MG" mousemove --window=%1 100 30 click 1 sleep 5 mousemove --window=%1 350 30 click 1 sleep 0.5 mousemove --window=%1 350 40 click 1 mousemove --window=%1 600 30 click 1 sleep 180 getwindowfocus windowkill
 
 # 	notifica ejecución a Notion
-../env/bin/python3.11 ../notificaciones/notificador.py -c "console" "notion" -m "Otra La Notificación del Programa de Computación" -o "up" -s "Precios MG en GBP"
+/home/inea/sistemas/inea_produccion/env/bin/python3.11 /home/inea/sistemas/inea_produccion/notificaciones/notificador.py -c "console" "notion" -m "Ejecución de Precios MG>GBP notificada" -o "up" -s "Precios MG en GBP"
 
 
 echo "script de actualización de precios ejecutado"
