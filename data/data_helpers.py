8394b7ac82dff01cbe4e440b7cb5136a2458b1ee
moregain909 <juan@gregor.com.ar>
2023-12-29 10:51:28 -0300

    Incorpora clase Database

--- data/data_helpers.py
+++ data/data_helpers.py
@@ -13,7 +13,24 @@ import os, sys
 path2root = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(path2root)
 
-#from precios_mg.precios_mg_helpers import ProductoMG
+#from precios_mg.precios_mg_helpers import CostoMG, DisponibilidadStock, ItemMG
+
+
+load_dotenv()
+
+# Database producción server INEA
+PRODUCCION_MYSQL_USER = os.getenv('INEA_DB_MYSQL_USER')
+PRODUCCION_MYSQL_PASS = os.getenv('INEA_DB_MYSQL_PASS')
+PRODUCCION_MYSQL_HOST = os.getenv('INEA_DB_MYSQL_HOST')
+PRODUCCION_MYSQL_PORT = os.getenv('INEA_DB_MYSQL_PORT')
+PRODUCCION_DB = os.getenv('INEA_DB_DATABASE')
+
+# Database de prueba local@local
+TEST_MYSQL_USER = os.getenv('INEA_TEST_DB_MYSQL_USER')
+TEST_MYSQL_PASS = os.getenv('INEA_TEST_DB_MYSQL_PASS')
+TEST_MYSQL_HOST = os.getenv('INEA_TEST_DB_MYSQL_HOST')
+TEST_MYSQL_PORT = os.getenv('INEA_TEST_DB_MYSQL_PORT')
+TEST_DB = os.getenv('INEA_TEST_DB_DATABASE')
 
 
 def db_connection(user: str, password: str, host: str, port: str, database: str, driver: str ="mysql+pymysql") -> Tuple:
@@ -37,8 +54,26 @@ def create_tables(engine: Engine) -> bool:
 class Base(DeclarativeBase):
     pass
 
+class Database:
+    """ Clase que permite crear y manejar una conection y un engine de una database"""
 
+    def __init__(self, db_location):
+        self.db_location = db_location
+        
+        #   Connect to db and start a session
+        if self.db_location == "local" or self.db_location == "desarrollo":
+            #   local test database
+            ce = db_connection(TEST_MYSQL_USER, TEST_MYSQL_PASS, TEST_MYSQL_HOST, TEST_MYSQL_PORT, TEST_DB)
+        else:
+            #   remote production database
+            ce = db_connection(PRODUCCION_MYSQL_USER, PRODUCCION_MYSQL_PASS, PRODUCCION_MYSQL_HOST, PRODUCCION_MYSQL_PORT, PRODUCCION_DB)
     
+        self.connection = ce[0]
+        self.engine = ce[1]
+
+
+
+
 if __name__ == "__main__":
 
 
