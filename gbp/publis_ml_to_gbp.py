2cf31df58635fe4202e25a733099829a85748a30
moregain909 <juan@gregor.com.ar>
2024-04-12 15:30:36 -0300

    edita data temporal irrelevante

--- gbp/publis_ml_to_gbp.py
+++ gbp/publis_ml_to_gbp.py
@@ -26,23 +26,21 @@ if __name__ == "__main__":
     #                       "MG Lenovo - Premium": 13"}
 
     tecnorium_items = [\
-        GbpMlItem(ml_item_id="1693647752", sku="AAF930", variation_id="", price_list_id=10, warehouse_id=1), \
-        GbpMlItem(ml_item_id="1693661174", sku="AAF930", variation_id="", price_list_id=10, warehouse_id=1)] \
-        #GbpMlItem(ml_item_id="1691589704", sku="31036", variation_id="", price_list_id=5, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1692075972", sku="31036", variation_id="", price_list_id=5, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691512376", sku="21E70004AC", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691499272", sku="PCI58GS7_W11_088", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691538426", sku="PCI58GS7_084", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691590110", sku="21E4001UAR", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691499426", sku="21EE0000AC", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1691473496", sku="21ECS0PU00", variation_id="", price_list_id=10, warehouse_id=1)] \
-        #GbpMlItem(ml_item_id="1407900325", sku="21H2000RAR", variation_id="", price_list_id=10, warehouse_id=1), \
-        #GbpMlItem(ml_item_id="1664701300", sku="AS9033PI", variation_id="", price_list_id=10, warehouse_id=1)] \
+        GbpMlItem(ml_item_id="880340924", sku="DS2208-SR7U2100SGW", variation_id="64723225935", price_list_id=10, warehouse_id=1), \
+        GbpMlItem(ml_item_id="880339686", sku="LS2208-SR20007R-UR", variation_id="174361829507", price_list_id=5, warehouse_id=1)] \
+        #GbpMlItem(ml_item_id="1701835556", sku="21061", variation_id="", price_list_id=5, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1701797126", sku="21049", variation_id="", price_list_id=1, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1701861314", sku="JL806A", variation_id="", price_list_id=10, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1414382543", sku="22994", variation_id="", price_list_id=1, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1701758288", sku="22976", variation_id="", price_list_id=1, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1414382635", sku="22976", variation_id="", price_list_id=5, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1701758400", sku="72373", variation_id="", price_list_id=10, warehouse_id=1), \
+        #GbpMlItem(ml_item_id="1701823122", sku="CBS110-8T-D-NA", variation_id="", price_list_id=10, warehouse_id=1)] \
         #GbpMlItem(ml_item_id="1664701300", sku="AS9033PI", variation_id="", price_list_id=10, warehouse_id=1) \                                                                                        
     
                         
     
-    lenovo_items = [GbpMlItem(ml_item_id="1693941824", sku="11JCS0K000/L", variation_id="", price_list_id=11, warehouse_id=1) \
+    lenovo_items = [GbpMlItem(ml_item_id="1708656186", sku="21HE0015AR", variation_id="", price_list_id=11, warehouse_id=1) \
                     #GbpMlItem(ml_item_id="1613175452", sku="21EE0001AC", variation_id="", price_list_id=11, warehouse_id=1), \
                     #GbpMlItem(ml_item_id="1691539830", sku="21HR000WAR", variation_id="", price_list_id=11, warehouse_id=1), \
                     #GbpMlItem(ml_item_id="1691487678", sku="21E70004AC", variation_id="", price_list_id=11, warehouse_id=1), \
@@ -56,8 +54,8 @@ if __name__ == "__main__":
                     #GbpMlItem(ml_item_id="1691526784", sku="21H2000RAR", variation_id="", price_list_id=11, warehouse_id=1) \
                     ]
     
-    celestron_items = [GbpMlItem(ml_item_id="1691487118", sku="31036", variation_id="", price_list_id=1, warehouse_id=1), \
-                       GbpMlItem(ml_item_id="1613175452", sku="31036", variation_id="", price_list_id=5, warehouse_id=1)] \
+    celestron_items = [GbpMlItem(ml_item_id="1708628996", sku="21049", variation_id="", price_list_id=5, warehouse_id=1)] \
+                       #GbpMlItem(ml_item_id="1613175452", sku="31036", variation_id="", price_list_id=5, warehouse_id=1)] \
                        #GbpMlItem(ml_item_id="1675643192", sku="22007", variation_id="", price_list_id=5, warehouse_id=1), \
                        #GbpMlItem(ml_item_id="1675656248", sku="21074", variation_id="", price_list_id=5, warehouse_id=1), \
                        # GbpMlItem(ml_item_id="1410806747", sku="21063", variation_id="", price_list_id=5, warehouse_id=1) \
@@ -66,8 +64,8 @@ if __name__ == "__main__":
     #! CONFIG TIENDA DE LAS PUBLIS A EXPORTAR 
     #!      IMPORTAR DE A UNA TIENDA POR VEZ 
     #TODO:  O ACTUALIZAR SCRIPT PARA QUE GENERE UN EXCEL POR CADA TIENDA
-    
-    item_list = lenovo_items
+
+    item_list = tecnorium_items
 
     for item in item_list:
         item.format_ml_item_id()
