a64468eae72777f250302612656d830ba1113cb0
moregain909 <juan@gregor.com.ar>
2024-04-12 15:15:18 -0300

    Registra blueprints sku_en_mg y callbacks_ml
    +app.register_blueprint(sku_en_mg_blueprint)
    +app.register_blueprint(callbacks_ml)

--- inea_app.py
+++ inea_app.py
@@ -6,12 +6,16 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from gbp.publis_ml_to_gbp_flask_blueprint import publis_ml_to_gbp_blueprint
 from notion.tarifarios_envios_notion_blueprint import tarifarios_envios_notion_blueprint
+from precios_mg.consulta_sku_al_ws_bp import sku_en_mg_blueprint
+from ml.redirect_uri_bp import callbacks_ml
 
 app = Flask(__name__)
 app.secret_key = "cantina"
 app.register_blueprint(publis_ml_to_gbp_blueprint)
 app.register_blueprint(tarifarios_envios_notion_blueprint)
-#app.register_blueprint(blueprint_example)
+app.register_blueprint(sku_en_mg_blueprint)
+app.register_blueprint(callbacks_ml)
+
 
 
 if __name__ == "__main__":
