919b2c534ef1328019025d0c62cb813ac9bd326c
moregain909 <juan@gregor.com.ar>
2024-04-26 16:15:35 -0300

    agrega gestion_publicaciones_blueprint

--- inea_app.py
+++ inea_app.py
@@ -8,6 +8,7 @@ from gbp.publis_ml_to_gbp_flask_blueprint import publis_ml_to_gbp_blueprint
 from notion.tarifarios_envios_notion_blueprint import tarifarios_envios_notion_blueprint
 from precios_mg.consulta_sku_al_ws_bp import sku_en_mg_blueprint
 from ml.redirect_uri_bp import callbacks_ml
+from ml.bp_gestion_publicaciones import gestion_publicaciones_blueprint
 
 app = Flask(__name__)
 app.secret_key = "cantina"
@@ -15,6 +16,7 @@ app.register_blueprint(publis_ml_to_gbp_blueprint)
 app.register_blueprint(tarifarios_envios_notion_blueprint)
 app.register_blueprint(sku_en_mg_blueprint)
 app.register_blueprint(callbacks_ml)
+app.register_blueprint(gestion_publicaciones_blueprint)
 
 
 
