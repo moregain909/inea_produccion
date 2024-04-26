7474a52fc921cde18e8eb5295e06163e114aa1bc
moregain909 <juan@gregor.com.ar>
2024-04-26 19:33:39 -0300

    gestiona sessiones ML para cada tienda ml

--- ml/bp_gestion_publicaciones.py
+++ ml/bp_gestion_publicaciones.py
@@ -11,6 +11,7 @@
 
 from flask import Flask, render_template, request, flash, send_file, Blueprint
 import logging
+from typing import List, Dict
 
 import os, sys
 path2root = os.path.join(os.path.dirname(__file__), "..")
@@ -20,22 +21,55 @@ from auth import MlSession
 
 gestion_publicaciones_blueprint = Blueprint('gestion_publicaciones', __name__, template_folder="templates")
 
-tiendas = ["tecnorium", "lenovo"]
+sessions: Dict = {}
 
-session = MlSession(store_name="tecnorium")
+items = None
 
 @gestion_publicaciones_blueprint.route("/gestion_publicaciones.html", methods=['GET', 'POST'])
 def gestion_publicaciones():
-    return render_template("gestion_publicaciones.html", items=["item1"])
+    if request.method == "GET":
+        print(items)
+        return render_template("gestion_publicaciones.html", items=items)
+    
+    elif request.method == "POST":
+        
+        # TODO: ALMACENAR CONFIGURACION DE FORM PARA PRESERVARLA EN EL RENDER DEL TEMPLATE
 
-# WEB: 
-# Filtro: Selección de tienda
-# Filtro: Estado publicaciones
-# Filtro: Está en GBP / No está en GBP
-# Filtro: Proveedor
-# Otros posibles filtros: tipo publicación, flex, retiro, garantía
-# Selección de campos a traer con checkbox
-# Botón TRAER PUBLICACIONES
+        # ASEGURA SESIONES PARA CADA STORE
+        store_names = request.form.getlist("requestConfigSeller")
+        
+        for store_name in store_names:
+            store_in_sessions = False
+            for session_store in sessions.keys():
+                if store_name == session_store:
+                    store_in_sessions = True
+                    break
+            if not store_in_sessions:
+                store_session = MlSession(store_name=store_name)
+                sessions.update({"store_name": store_session})
+            
+
+        #   CONFIGURA REQUEST
+        #   REQUEST ITEM ID: requestConfigState
+        #
+        print(request.form.to_dict(flat=False))
+        print(request.form.keys())
+        
+        
+        # PARA CADA STORE, ENVIA REQUEST
+        # requestConfigSeller
+        # requestConfigState
+        # requestConfigFields
+
+        # query
+        # queryConfigIsInGbp
+        # queryConfigSuppliers
+
+        # levantar los parámetros del request
+        # llamar a la api
+        # generar lista de publicaciones
+        return render_template("gestion_publicaciones.html", items=["item1"], form=request.form)
+        
 
 
 # traer_ID_publicaciones_inactivas_de_tienda
@@ -46,3 +80,7 @@ def gestion_publicaciones():
 # V1: BOTON BORRAR en cada una, y BOTON BORRAR TODAS
 # V2: checkbox en cada una borrar
 # Una vez que se borra, vuelve a mostrar el listado actualizado
+
+
+if __name__ == "__main__":
+    pass
