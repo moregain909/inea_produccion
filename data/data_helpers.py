c96aff505c378eb4a01b663c1d527027d268802f
moregain909 <juan@gregor.com.ar>
2024-05-21 15:18:04 -0300

    Las clases Base de db pasan a data_helpers.py
    Se mueven GbpSkuMlItem(Base) y GbpSeveralTables_DbItem(Base)
        de gbp_helpers a data_helpers
    
    Con esto se evita el bug que el error InvalidRequestError:
    Table 'gbp_sku_ml_item' is already defined for this MetaData instance.

--- data/data_helpers.py
+++ data/data_helpers.py
@@ -8,6 +8,7 @@ from sqlalchemy.sql import func
 from datetime import datetime, time
 from typing import Tuple
 import requests
+import logging
 
 import os, sys
 path2root = os.path.join(os.path.dirname(__file__), "..")
@@ -15,6 +16,7 @@ sys.path.append(path2root)
 
 #from precios_mg.precios_mg_helpers import CostoMG, DisponibilidadStock, ItemMG
 
+logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 
@@ -79,6 +81,105 @@ class Base(DeclarativeBase):
         except Exception as e:
             print(f'Error al insertar el producto {self}: {e}')
             return False
+
+
+
+class GbpSkuMlItem(Base):
+    """ Clase que representa un registro de la tabla gbp_sku_ml_item, que almacena los skus que contiene cada publi ML
+    """
+
+    __tablename__ = "gbp_sku_ml_item"
+
+    ml_item_id = Column(String (20), primary_key=True, index=True)
+    sku = Column(String (100))
+
+    def __init__(self, **kwargs):
+        
+        if kwargs:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    
+    def __repr__(self):
+        return f"id_ml: {self.ml_item_id}, sku: {self.sku}"
+    
+
+    def get_data_from_gbp_item(self, gbp_ml_item):
+        
+        class_table = self.__table__
+        attributes = class_table.columns.keys()
+        
+        for attribute in attributes:
+            if attribute in gbp_ml_item.__dict__.keys():
+                setattr(self, attribute, getattr(gbp_ml_item, attribute))
+
+        
+    def is_on_db(self, session):
+        
+        sku_on_db = session.query(GbpSkuMlItem).filter(GbpSkuMlItem.ml_item_id == self.ml_item_id).first()
+        
+        if sku_on_db:
+            return True
+        return False
+    
+    def insert_into_db(self, session):
+        
+        try:
+            session.add(self)
+            session.commit()
+            print(f'Producto {self.sku} insertado correctamente.')
+            session.close()
+            return True
+        
+        except IntegrityError as e:
+            print(f'Error al insertar el producto {self}: {e}')
+            return False            
+        
+        except Exception as e:
+            print(f'Error al insertar el producto {self}: {e}')
+            return False
+
+
+class GbpSeveralTables_DbItem(Base):
+    """ Clase que representa un registro de la tabla gbp_several_tables, que almacena data de distintas tablas de GBP:\n
+    Lista de precios\n
+    Lista de costos\n
+    Depósitos\n
+    Categorias\n
+    Subcategorías\n
+    Subcategoria Auxiliar\n
+    Marcas\n
+    Monedas\n
+    Tiendas ML\n
+    """
+
+    __tablename__ = "gbp_several_tables"
+
+    db_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    gbp_table = Column(String (50))
+    gbp_id = Column(String (20))
+    name = Column(String (100))
+    extra = Column(String (100))
+
+    def __init__(self, **kwargs):
+        
+        if kwargs:
+            for k, v in kwargs.items():
+                setattr(self, k, v)    
+
+    def __repr__(self):
+        return f"db_id: {self.db_id}, table: {self.gbp_table}, gbp_id: {self.gbp_id}, name: {self.name}, extra: {self.extra}"
+    
+    def is_on_db(self, session):
+        
+        item_on_db = session.query(GbpSeveralTables_DbItem).filter(GbpSeveralTables_DbItem.gbp_id == self.gbp_id, GbpSeveralTables_DbItem.gbp_table == self.gbp_table).first()
+        
+        if item_on_db:
+            logging.debug(f'{self.name} gbp_id {self.gbp_id} in on db already')
+            return True
+        logging.debug(f'{self.name} gbp_id {self.gbp_id} not in on db')
+        return False
+    
 
 
 class Database:
